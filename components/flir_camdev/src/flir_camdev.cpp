622c3fc16b2ddee20e8188b5a431af749cb7be49
Nathan Pennie <kb1rd@kb1rd.net>
2019-05-14 18:04:28 -0400

    Set up command line parser for FLIR driver

--- components/flir_camdev/src/flir_camdev.cpp
+++ components/flir_camdev/src/flir_camdev.cpp
@@ -1,8 +1,24 @@
 #include <stdio.h>
-
-#include "flir_camdev.h"
-#include "MemoryMappedObject.h"
+#include <tclap/CmdLine.h>
 
 int main(int argc, char** argv) {
-  MemoryMappedObject<struct flir_camdev> mmo("./test", CREATE);
+	try {
+		TCLAP::CmdLine cmd("Device driver for FLIR Lepton", ' ', "0.1A");
+		TCLAP::UnlabeledValueArg<std::string> uart_file_arg("uart",
+																												"UART file to open",
+																												true, "","uart");
+		TCLAP::UnlabeledValueArg<std::string> video_file_arg("video",
+																												"Video file to open",
+																												true, "","video");
+		
+		cmd.add(uart_file_arg);
+		cmd.add(video_file_arg);
+		cmd.parse(argc, argv);
+		
+		std::string uart_file_name = uart_file_arg.getValue();
+		std::string video_file_name = video_file_arg.getValue();
+		std::cout << uart_file_name << video_file_name << std::endl;
+	} catch (TCLAP::ArgException &e) {
+		printf("error: %s for arg %s\n", e.error().c_str(), e.argId().c_str());
+	}
 }
